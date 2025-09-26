9771b770f163f3398e6e06ee1596f71bcf2eb743
ENUGANTI KARTHIK <enugantikarthikyadav@gmail.com>
2025-09-26 17:33:14 +0530

    Update index.js

--- new backend/index.js
+++ new backend/index.js
@@ -11,12 +11,12 @@ const JWT_SECRET = "karthik";
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5073", 
+  origin: "onlinecourse-fu2y.vercel.app", 
   credentials: true
 }));
 
 const corsOptions = {
-  origin: "http://localhost:5073",
+  origin: "onlinecourse-fu2y.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
